d730035f03863a47cb6bdc302c5130c224548937
aleksandrail <54113006+aleksandrail@users.noreply.github.com>
2019-08-15 19:00:22 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,14 +1,34 @@
-var Discord = require('discord.js');
-var bot = new Discord.Client();
+const { Client } = require("discord.js");
+const { config } = require("dotenv");
 
-bot.on('ready', () => {
-    bot.user.setStatus('available') // Can be 'available', 'idle', 'dnd', or 'invisible'
-    bot.user.setPresence({
-        game: {
-            name: 'Type !help',
-            type: 0
-        }
-    });
+// Declares our bot,
+// the disableEveryone prevents the client to ping @everyone
+const client = new Client({
+    disableEveryone: true
 });
+
+config({
+    path: __dirname + "/.env"
+})
+
+// When the bot's online, what's in these brackets will be executed
+client.on("ready", () => {
+    console.log(`Hi, ${client.user.username} is now online!`);
+
+    // Set the user presence
+    client.user.setPresence({
+        status: "online",
+        game: {
+            name: "Cracking",
+            type: "WATCHING"
+        }
+    }); 
+})
+
+// When a message comes in, what's in these brackets will be executed
+client.on("message", async message => {
+    console.log(`${message.author.username} said: ${message.content}`);
+});
+
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
