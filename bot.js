301a815729780710a4b2dafa1a45301ad457af63
aleksandrail <54113006+aleksandrail@users.noreply.github.com>
2019-08-15 18:53:41 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,8 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+const bot = new Discord.Client({disableEveryone: true});
+
 client.on('ready', () => {
     console.log('I am ready!');
   bot.user.setActivity("Cracking", {type: "WATCHING"});    
