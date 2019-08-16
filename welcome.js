13fb0d74a20541602f6d4b7afd920d470a1c6b3c
aleksandrail <54113006+aleksandrail@users.noreply.github.com>
2019-08-16 13:42:18 +0200

    Update welcome.js

--- welcome.js
+++ welcome.js
@@ -1,14 +1,10 @@
 client.on('guildMemberAdd', member => {
 client.on('message', 
 
-
-var role = member.guild.roles.find('name', 'Beginner role name'); // Variable to get channel ID
-member.addRole(role); // Adds the default role to members
-
 member.guild.channels.get('603540390917701632').send({embed: {
 color: 3447003,
 title: "**World Cracking** Welcome!",
-url: "WEBSITE URL",
+url: "WorldCracking.ga",
 description: "Welcome *" + member + "* to the **World Cracking** discord server!",
 fields: [{
     name: "Information",
