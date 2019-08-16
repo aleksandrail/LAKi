3707885e7ed3560408f6c6adc34cb265368e4dcb
aleksandrail <54113006+aleksandrail@users.noreply.github.com>
2019-08-16 02:10:10 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,7 +8,7 @@ const client = new Client({
 
 // When the bot's online, what's in these brackets will be executed
 client.on("ready", () => {
-    console.log(`Hi, ${client.user.username} is now online!`);
+    console.log(`${client.user.username} Je Online!`);
 
     // Set the user presence
     client.user.setPresence({
@@ -22,21 +22,6 @@ client.on("ready", () => {
 // When a message comes in, what's in these brackets will be executed
 client.on("message", async message => {
     console.log(`${message.author.username} said: ${message.content}`);
-});
-  if(cmd === "${prefix}serverinfo`){
-
-    let sicon = message.guild.iconURL;
-    let serverembed = new Discord.RichEmbed()
-    .setDescription("Server Information")
-    .setColor("#15f153")
-    .setThumbnail(sicon)
-    .addField("Server Name", message.guild.name)
-    .addField("Created On", message.guild.createdAt)
-    .addField("You Joined", message.member.joinedAt)
-    .addField("Total Members", message.guild.memberCount);
-
-    return message.channel.send(serverembed);
-  }
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
